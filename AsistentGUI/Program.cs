0e8e342cd77e4f7f6a71b7088b13a08e043911a0
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-04-28 19:26:58 +0200

    TextConnection

--- AsistentGUI/Program.cs
+++ AsistentGUI/Program.cs
@@ -16,7 +16,7 @@ namespace AsistentGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            TurnirAsistentModel.GlobalConfig.InitalizeConnectins(TurnirAsistentModel.DatabaseType.Sql);
+            TurnirAsistentModel.GlobalConfig.InitalizeConnectins(TurnirAsistentModel.DatabaseType.TextFile);
             Application.Run(new Stvaranje_nagrade());
             // Application.Run(new PrijavaTurnir());
         }
